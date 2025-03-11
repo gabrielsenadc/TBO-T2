589e674997681431d7bd162badb7867a9ed62619
gabrielsenadc <gabriel.sena.cunha@gmail.com>
2025-03-11 20:49:45 -0300

    Consertando erros de remoção

--- BT.c
+++ BT.c
@@ -63,7 +63,7 @@ node_type * node_get_sibling(node_type * node, node_type * parent, int right){
     if(right) i++;
     else i--;
 
-    if(i >= parent->size) return NULL;
+    if(i > parent->size) return NULL;
     if(i < 0) return NULL;
 
     return parent->children[i];
@@ -78,6 +78,10 @@ void node_concat(node_type * dest, node_type * src){
     }
     dest->size += src->size;
     dest->children[dest->size] = src->children[src->size];
+
+    for(int i = 0; i <= src->size; i++){
+        src->children[i] = NULL;
+    }
 
 }
 
@@ -223,15 +227,15 @@ int BT_search(node_type * root, int key) {
 
 node_type * remove_key(BT_type * BT, node_type * node, int key);
 
-node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int key){
+node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int i_parent){
     if(node->size >= BT->order/2) return parent;
 
     node_type * left_sibling = node_get_sibling(node, parent, 0);
     node_type * right_sibling = node_get_sibling(node, parent, 1);
 
     if(node_get_size(left_sibling) > BT->order/2){
-        int i_parent = 0;
-        for(; i_parent < parent->size; i_parent++) if(key < parent->keys[i_parent]) break;
+        /*int i_parent = 0;
+        for(; i_parent < parent->size; i_parent++) if(key < parent->keys[i_parent]) break;*/
         i_parent--;
 
         node->size++;
@@ -256,8 +260,8 @@ node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int ke
 
 
     } else if(node_get_size(right_sibling) > BT->order/2){
-        int i_parent = 0;
-        for(; i_parent < parent->size; i_parent++) if(key < parent->keys[i_parent]) break;
+        /*int i_parent = 0;
+        for(; i_parent < parent->size; i_parent++) if(key < parent->keys[i_parent]) break;*/
 
         node->size++;
 
@@ -281,8 +285,8 @@ node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int ke
         right_sibling->size--;
 
     } else {
-        int i_parent = 0;
-        for(; i_parent < parent->size; i_parent++) if(key < parent->keys[i_parent]) break;
+        /*int i_parent = 0;
+        for(; i_parent < parent->size; i_parent++) if(key <= parent->keys[i_parent]) break;*/
         if(right_sibling == NULL) i_parent--;
 
         int key_parent = parent->keys[i_parent];
@@ -299,8 +303,8 @@ node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int ke
             left_sibling->size++;
             left_sibling->children[node->size] = NULL;
 
-            left_sibling->keys[0] = parent->keys[i_parent];
-            left_sibling->values[0] = parent->values[i_parent];
+            left_sibling->keys[left_sibling->size - 1] = parent->keys[i_parent];
+            left_sibling->values[left_sibling->size - 1] = parent->values[i_parent];
 
             node_concat(left_sibling, node);   
             node_free(node);
@@ -323,7 +327,7 @@ node_type * fix_caso3(BT_type * BT, node_type * parent, node_type * node, int ke
     return parent;
 }
 
-void remove_key_caso1(BT_type * BT, node_type * node, int key){
+node_type *  remove_key_caso1(BT_type * BT, node_type * node, int key){
     for(int i = 0; i < node->size; i++){
         if(node->keys[i] > key){
             node->keys[i - 1] = node->keys[i];
@@ -332,6 +336,8 @@ void remove_key_caso1(BT_type * BT, node_type * node, int key){
     }
 
     node->size--;
+
+    return node;
 }
 
 node_type * remove_key_caso2(BT_type * BT, node_type * node, int key){
@@ -342,21 +348,25 @@ node_type * remove_key_caso2(BT_type * BT, node_type * node, int key){
         node_type * righter = node_get_righter(node->children[i]);
         int j = node_get_size(righter) - 1;
 
+        int new_key = righter->keys[j];
+
         node->keys[i] = righter->keys[j];
         node->values[i] = righter->values[j];
 
-        remove_key(BT, node->children[i], righter->keys[j]);
-        return fix_caso3(BT, node, node->children[i], key);
+        node->children[i] = remove_key(BT, node->children[i], righter->keys[j]);
+        return fix_caso3(BT, node, node->children[i], i);
 
     } else {
         node_type * lefter = node_get_lefter(node->children[i + 1]);
         int j = 0;
 
+        int new_key = lefter->keys[j];
+
         node->keys[i] = lefter->keys[j];
         node->values[i] = lefter->values[j];
 
-        remove_key(BT, node->children[i + 1], lefter->keys[j]);
-        return fix_caso3(BT, node, node->children[i + 1], key);
+        node->children[i + 1] = remove_key(BT, node->children[i + 1], lefter->keys[j]);
+        return fix_caso3(BT, node, node->children[i + 1], i);
     }
 
 }
@@ -366,7 +376,7 @@ node_type * remove_key(BT_type * BT, node_type * node, int key){
     for(; i < node->size; i++) if(key <= node->keys[i]) break;
 
     if(i < node->size && key == node->keys[i]){
-        if(node->leaf) remove_key_caso1(BT, node, key);
+        if(node->leaf) node = remove_key_caso1(BT, node, key);
         else node = remove_key_caso2(BT, node, key);
 
         return node;
@@ -375,7 +385,7 @@ node_type * remove_key(BT_type * BT, node_type * node, int key){
     if(node->leaf) printf("NAO EXISTE");
     else remove_key(BT, node->children[i], key);
 
-    return fix_caso3(BT, node, node->children[i], key);
+    return fix_caso3(BT, node, node->children[i], i);
 }
 
 void BT_remove(BT_type * BT, int key){
@@ -395,8 +405,11 @@ void BT_print(BT_type * BT){
         for(int j = 0; j < i; j++){
             node = dequeue(queue);
             node_print(node);
-            if(node_get_size(node) > 0) {
-                for(int i = 0; i <= node_get_size(node); i++) enqueue(queue, node->children[i]);
+            if(node_get_size(node) > 0 && !node->leaf) {
+                for(int k = 0; k <= node_get_size(node); k++){
+                    node_type * add = node->children[k];
+                    enqueue(queue, add);
+                } 
                 qtt_filhos += node_get_size(node) + 1;
             }
             if(j != i - 1) printf(" ");
